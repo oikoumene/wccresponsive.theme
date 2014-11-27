0b5cebff9e171f0a8c7083583c43288dd05d18c8
joemariedimzon <joemariedimzon@gmail.com>
2014-11-27 11:09:34 +0800

    added files from old theme

---
+++ wccresponsive/theme/browser/scheme.py
@@ -0,0 +1,22 @@
+from five import grok
+from zope.interface import Interface
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from wccresponsive.theme.interfaces import IThemeSettings
+from wccresponsive.theme.vocabulary import SCHEMES_CSS
+from zope.component.hooks import getSite
+#from wccresponsive.theme import getSettings
+
+class SchemeCSS(grok.View):
+    grok.context(Interface)
+    grok.name('wccresponsive-theme-scheme.css')
+
+    def render(self):
+        #settings = getSettings()
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IThemeSettings)
+        self.request.response.setHeader('Content-Type','text/css; charset=utf-8')
+        scheme_css = SCHEMES_CSS[settings.scheme]
+        site = getSite()
+        result = getattr(site, scheme_css)
+        return unicode(result)
