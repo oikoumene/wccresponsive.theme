6abd383d0de6f6eb26c4a0c7f2be6d63dcbdd1d7
joemariedimzon <joemariedimzon@gmail.com>
2015-01-16 16:08:50 +0800

    rules/js adjustment

--- wccresponsive/theme/static/js/app.js
+++ wccresponsive/theme/static/js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+$("#portal-globalnav").tinyNav({
+    active: 'selected', // String: Set the "active" class
+    header: 'Navigation', // String: Specify text for "header" and show header instead of the active item
+    indent: '- ', // String: Specify text for indenting sub-items
+    label: '' // String: Sets the 
+});
+    
+jssor_slider1_starter('slider1_container');
